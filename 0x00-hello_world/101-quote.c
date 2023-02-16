db7423ef199ab4bed9b7b286a3a81b4dcf68b3f9
Ti-Ray <iannjoroge25@gmail.com>
2023-02-16 14:01:00 -0800

    Starting on c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-	puts("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	write(STDOUT_FILENO, "and that piece of art is useful" - Dora Korpar, 2015-10-19");
 	return (1);
 }
