eb9b9969530c6b5ac0457ca326a4b30d62a20fa3
Ti-Ray <iannjoroge25@gmail.com>
2023-02-16 11:12:23 -0800

    Starting on c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <stdio.h> 
+#include <stdio.h>
 /*This code is to print specific text.*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        return (0);
 }
