4968e9f55b0eec1868af84b37bba25dbb50832c9
Ti-Ray <iannjoroge25@gmail.com>
2023-02-17 07:09:27 -0800

    Variables If Else While

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -This is to print all numbers from 0-9
+ *
+ * Return: return(0)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
