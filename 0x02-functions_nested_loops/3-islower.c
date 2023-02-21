0ee71c8a531cc54fc17cb783d04ae8c840e1fd1f
Ti-Ray <iannjoroge25@gmail.com>
2023-02-21 13:06:38 -0800

    Functions and nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,14 +6,14 @@
  */
 int _islower(int c)
 {
-    int r = c;
+    int c;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
+    c = _islower('H');
+    _putchar(c + '0');
+    c = _islower('o');
+    _putchar(c + '0');
+    c = _islower(108);
+    _putchar(c + '0');
     _putchar('\n');
     return (0);
 }
