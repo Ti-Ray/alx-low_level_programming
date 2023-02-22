31d0ea4db2481f0870b71f6f132a9bae0fe8d119
Ti-Ray <iannjoroge25@gmail.com>
2023-02-22 11:09:33 -0800

    Functions and nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,34 +11,38 @@ void jack_bauer(void)
 	int m1;
 	int h1;
 
-	for (h = 0; h < 3; h++)
+	for (h = '0'; h < '3'; h++)
 	{
 		_putchar(h);
 		_putchar(h1);
 		_putchar(':');
 		_putchar(m);
 		_putchar(m1);
-		for (h1 = 0; h1 < 10; h1++)
+		_putchar('\n');
+		for (h1 = '0'; h1 < '10'; h1++)
 		{
 			_putchar(h);
 			_putchar(h1);
 			_putchar(':');
 			_putchar(m);
 			_putchar(m1);
-			for (m = 0; m < 6; m++)
+			_putchar('\n');
+			for (m = '0'; m < '6'; m++)
 			{
 				_putchar(h);
 				_putchar(h1);
 				_putchar(':');
 				_putchar(m);
 				_putchar(m1);
-				for (m1 = 0; m1 < 10; m1++)
+				_putchar('\n');
+				for (m1 = '0'; m1 < '10'; m1++)
 				{
 					_putchar(h);
 					_putchar(h1);
 					_putchar(':');
 					_putchar(m);
 					_putchar(m1);
+					_putchar('\n');
 				}
 			}
 		}
