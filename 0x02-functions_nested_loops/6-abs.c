0770fe2636194c837449765661bf02940e1e2595
Ti-Ray <iannjoroge25@gmail.com>
2023-02-22 09:41:14 -0800

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs -This is to check for absolute vaules
+ * 
+ * Return: Always 0 (success)
+ */
+int _abs(int)
+{
+	int m;
+	if (abs(m) > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
