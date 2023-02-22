49b299d065e61b28ee4cb6d226b63fb4a4209ca0
Ti-Ray <iannjoroge25@gmail.com>
2023-02-22 09:59:02 -0800

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit -This is to print the last digit of a number
+ * @v: This is the var for the last number
+ * Return: Always 0 (success)
+ */
+int print_last_digit(int v)
+{
+	int num;
+	num = v % 10;
+	return (num);
+}
