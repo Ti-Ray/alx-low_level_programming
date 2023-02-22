fdd1628033c675f1469ce0a520b04d34ae933222
Ti-Ray <iannjoroge25@gmail.com>
2023-02-22 09:03:33 -0800

    Functions and nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower -This one displays one is c is lowercase
- *
+ *@c: is passed as the holder of the variable
  * Return: Always 0 (success)
  */
 int _islower(int c)
