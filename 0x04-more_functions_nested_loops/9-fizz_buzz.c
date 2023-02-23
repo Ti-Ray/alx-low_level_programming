c0fbf6bc2b3e84a75109faaf283c38a69ed1aac1
Ti-Ray <iannjoroge25@gmail.com>
2023-02-22 21:40:02 -0800

    Prints all numbers from 1-100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main -This is to print all values from '1' to '100' with Fizz in place of multiples of three
+ *
+ * Return: Always 0
+ */
+int main (int i)
+{
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			putchar('F');
+			putchar('i');
+			putchar('z');
+			putchar(' ');
+		}
+		else if ((i % 5) == 0)
+		{
+			putchar('B');
+			putchar('u');
+			putchar('z');
+			putchar('z');
+			putchar(' ');
+		}
+		else if ((i % 5) == 0 && (i % 3) == 0)
+		{
+			putchar('F');
+			putchar('i');
+			putchar('z');
+			putchar('z');
+			putchar('B');
+			putchar('u');
+			putchar('z');
+			putchar('z');
+			putchar(' ');
+		}
+		putchar('0' + i);
+	}
+}
